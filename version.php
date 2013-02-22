4238a0eb473f120f9219da8b3596b270b5a1791b
Thom W. Rawson <thomw@sun.ac.jp>
2013-02-22 10:05:24 +0900

    Latest changes added from the official poodll mercurial repositories.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013012900; 
+$plugin->version   = 2013021900; 
 $plugin->requires  = 2011070100.00; 
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_BETA;
