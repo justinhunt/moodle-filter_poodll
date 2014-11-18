4d5c7c4e53a4bfa5f1805b73e5104c7eac7b7f4f
Justin Hunt <poodllsupport@gmail.com>
2014-11-18 16:05:17 +0900

    added a few more strings and tweaked a bug or two

--- lang/de/filter_poodll.php
+++ lang/de/filter_poodll.php
@@ -1,10 +1,9 @@
 <?PHP // $Id: filter_poodll.php ,v 1.3 2012/05/16 12:47:13 Justin Hunt Exp $ 
       // PoodLl Filter
-
 $string['recui_record'] ='Aufnahme';
 $string['recui_pause'] ='Pause';
 $string['recui_play'] ='Wiedergabe';
-$string['recui_continue'] ='Continue';
+$string['recui_continue'] ='Weiter';
 $string['recui_stop'] ='Stop';
 $string['recui_time'] ='Dauer:';
 $string['recui_audiogain'] ='Lautstärke';
@@ -16,9 +15,8 @@ $string['recui_on'] ='An';
 $string['recui_off'] ='Aus';
 $string['recui_ok'] ='OK';
 $string['recui_close'] ='Schließen';
-$string['recui_continue'] ='Continue';
-$string['recui_uploading'] ='uploading';
-$string['recui_converting'] ='converting';
-$string['recui_inaudibleerror'] ='We can not hear you. Please check flash and browser permissions.';
+$string['recui_uploading'] ='Hochladen';
+$string['recui_converting'] ='Konvertierung';
+$string['recui_inaudibleerror'] ='Wir können Sie nicht hören. Bitte überprüfen Sie die Flash- und Browserberechtigungen.';
 $string['recui_timeouterror'] ='Zeitüberschreitung der Anforderung';
 $string['recui_uploaderror'] ='Ein Fehler ist aufgetreten und Ihre Datei wurde NICHT hochgeladen.';
--- poodllresourcelib.php
+++ poodllresourcelib.php
@@ -2978,7 +2978,8 @@ function filter_poodll_fetch_recorder_strings(){
 	//Get localised labels: 
 	$params['ui_record'] = get_string('recui_record', 'filter_poodll');
 	$params['ui_play'] = get_string('recui_play', 'filter_poodll');
-	$params['ui_stop'] = get_string('recui_pause', 'filter_poodll');
+	$params['ui_continue'] = get_string('recui_continue', 'filter_poodll');
+	$params['ui_pause'] = get_string('recui_pause', 'filter_poodll');
 	$params['ui_stop'] = get_string('recui_stop', 'filter_poodll');
 	$params['ui_time'] = get_string('recui_time', 'filter_poodll');
 	$params['ui_audiogain'] = get_string('recui_audiogain', 'filter_poodll');
@@ -2990,8 +2991,11 @@ function filter_poodll_fetch_recorder_strings(){
 	$params['ui_off'] = get_string('recui_off', 'filter_poodll');
 	$params['ui_ok'] = get_string('recui_ok', 'filter_poodll');
 	$params['ui_close'] = get_string('recui_close', 'filter_poodll');
+	$params['ui_uploading'] = get_string('recui_uploading', 'filter_poodll');
+	$params['ui_converting'] = get_string('recui_converting', 'filter_poodll');
 	$params['ui_timeouterror'] = get_string('recui_timeouterror', 'filter_poodll');
 	$params['ui_uploaderror'] = get_string('recui_uploaderror', 'filter_poodll');
+	$params['ui_inaudibleerror'] = get_string('recui_inaudibleerror', 'filter_poodll');
 	
 	return $params;
 }
