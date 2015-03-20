8fc5001a1ed0635c356cfea6a0e93ec90d5b3147
Justin Hunt <poodllsupport@gmail.com>
2015-03-20 10:16:30 +0900

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014121701; 
+$plugin->version   = 2015031901; 
 $plugin->requires  = 2011070100.00; 
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
