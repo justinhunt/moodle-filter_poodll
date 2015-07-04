0016e843a46f49f77dc8340330f789ab61df8868
Justin Hunt <poodllsupport@gmail.com>
2015-07-04 02:53:37 -0400

    stupidly released before I had finished testing, this should fix issues with swf not loading

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015070201; 
+$plugin->version   = 2015070401; 
 $plugin->requires  = 2011070100.00; 
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.7.5(Build 2015070201)';
+$plugin->release   = '2.7.6(Build 2015070401)';
