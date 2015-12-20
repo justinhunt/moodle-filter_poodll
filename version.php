121950e1cc711a8e69123726690aa99914692df4
Justin Hunt <poodllsupport@gmail.com>
2015-12-20 17:29:21 -0500

    Bumped version for bug fix release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015121101;
+$plugin->version   = 2015121102;
 $plugin->requires  = 2015051100;
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.8.0(Build 2015121101)';
+$plugin->release   = '2.8.1(Build 2015121102)';
