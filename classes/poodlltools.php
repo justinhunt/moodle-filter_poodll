44e0db146c72b8e2e554e8060c7ca4f90c4627c2
gabrielrosset <gabriel.rosset@gmail.com>
2016-01-04 17:20:43 +0100

    fix fatal error while using HTML5 MP3 recorder
    fix a fatal error while using skinned HTML5 MP3 recorder
    core static class html_writer needs to be called outside of current namespace

--- classes/poodlltools.php
+++ classes/poodlltools.php
@@ -468,7 +468,7 @@ class poodlltools
 		$height = "170";
 		//$params['callbackjs']= 'poodll_audiosdk.audiohelper.poodllcallback';
 		$iframe_src_url = new \Moodle_URL("/filter/poodll/mp3recorderskins/$skin/index.php", $params);
-		$ret = html_writer::tag('iframe', '', array('src' => $iframe_src_url->out(false), 'frameBorder' => 0, 'scrolling' => 'none', 'allowTransparency' => 'true', 'class' => 'filter_poodll_mp3skinned_recorder'));
+		$ret = \html_writer::tag('iframe', '', array('src' => $iframe_src_url->out(false), 'frameBorder' => 0, 'scrolling' => 'none', 'allowTransparency' => 'true', 'class' => 'filter_poodll_mp3skinned_recorder'));
 		return $ret;
 
 
