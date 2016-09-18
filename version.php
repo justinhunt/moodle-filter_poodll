5c1f19d74d2b42454daff851715b510a1e5e15a1
Justin Hunt <poodllsupport@gmail.com>
2016-09-18 17:59:47 -0400

    bumped version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016081407;
+$plugin->version   = 2016081408;
 $plugin->requires  = 2016052300;//moodle 3.1.0
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.0.6(Build 2016081407)';
+$plugin->release   = '3.0.7(Build 2016081408)';
