922671b2c461c0f9aaac26b2592620edb3e7d1b5
Justin Hunt <poodllsupport@gmail.com>
2016-11-28 01:24:04 -0500

    fixed a problem with missing html and output js being out of sync leading to js errors

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016112801;
+$plugin->version   = 2016112802;
 $plugin->requires  = 2016052300;//moodle 3.1.0
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.0.19(Build 2016112801)';
+$plugin->release   = '3.0.20(Build 2016112802)';
