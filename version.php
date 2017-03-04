dd916e2fd57bcd8deb3f931e610d600e04e3b8bc
justinhunt <bitwalkerjapan@gmail.com>
2017-03-04 16:42:21 +0900

    version bump ahead of release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017020401;
+$plugin->version   = 2017030401;
 $plugin->requires  = 2016052300;//moodle 3.1.0
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.0.28(Build 2017020401)';
+$plugin->release   = '3.0.30(Build 2017030401)';
