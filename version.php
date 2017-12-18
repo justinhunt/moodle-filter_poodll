0f9bbb2b5c7030731745807692ba42fb97c164cb
iShineGuy <ishineguy@iShineGuys-MacBook-Pro.local>
2017-12-18 16:03:54 +0900

    version bump ahead of release

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   =  2017120201;
+$plugin->version   =  2017121801;
 $plugin->requires  = 2016052300;//moodle 3.1.0
 $plugin->component = 'filter_poodll'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.0.58(Build 2017120201)';
+$plugin->release   = '3.0.59(Build 2017121801)';
