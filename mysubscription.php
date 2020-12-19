61f6379416fa4422817f401bf94e8915bb668931
justinhunt <bitwalkerjapan@gmail.com>
2020-12-19 21:56:25 +0900

    i

--- mysubscription.php
+++ mysubscription.php
@@ -32,13 +32,9 @@ $mysubscriptions_names = array();
 
 foreach($mysubdata->usersubs as $subdata){
     $subscription_name = ($subdata->subscriptionname == ' ') ? "na" : strtolower(trim($subdata->subscriptionname));
-    $subscription_name = str_replace(array('-','(',')',' ','__'),'_',$subscription_name);
-    $subscription_name = 'ppn_'.$subscription_name;
-    $subscription_name = rtrim($subscription_name, "_");
-    
-    $mysubscription_name_txt[] = get_string($subscription_name,'filter_poodll');
-    $mysubscriptions_names[] = get_string($subscription_name,'filter_poodll'); 
-    $mysubscriptions[] = array('name'=>get_string($subscription_name,'filter_poodll'),'start_date'=>date("m-d-Y", $subdata->timemodified),'end_date'=>date("m-d-Y", $subdata->expiredate));
+    $mysubscription_name_txt[] = $subscription_name;
+    $mysubscriptions_names[] = $subscription_name;
+    $mysubscriptions[] = array('name'=>$subscription_name,'start_date'=>date("m-d-Y", $subdata->timemodified),'end_date'=>date("m-d-Y", $subdata->expiredate));
 }
 
 $reportdata['subscription_check'] = false;
@@ -49,7 +45,6 @@ if(count($mysubscriptions)>0){
 }
 
 $reportdata['subscriptions']=$mysubscriptions;
-
 $reportdata['pusers']=array();
 $reportdata['record']=array();
 $reportdata['recordmin']=array();
@@ -92,7 +87,7 @@ foreach($mysubdata->usersubs_details as $subdatadetails){
        $threesixtyfive_recordtype_audio += $subdatadetails->audio_file_count;
        $threesixtyfive_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
        $threesixtyfive_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-       $threesixtyfive_puser += $subdatadetails->puser_count;
+       $threesixtyfive_puser .= $subdatadetails->pusers;
    }
    
    if(($timecreated > strtotime('-90 days'))&&($timecreated <= strtotime('-180 days'))){
@@ -100,7 +95,7 @@ foreach($mysubdata->usersubs_details as $subdatadetails){
        $oneeighty_recordtype_audio += $subdatadetails->audio_file_count;
        $oneeighty_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
        $oneeighty_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-       $oneeighty_puser += $subdatadetails->puser_count;
+       $oneeighty_puser .= $subdatadetails->pusers;
    }
    
    if(($timecreated > strtotime('-30 days'))&&($timecreated <= strtotime('-90 days'))){
@@ -108,7 +103,7 @@ foreach($mysubdata->usersubs_details as $subdatadetails){
        $ninety_recordtype_audio += $subdatadetails->audio_file_count;
        $ninety_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
        $ninety_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-       $ninety_puser += $subdatadetails->puser_count;
+       $ninety_puser .= $subdatadetails->pusers;
  }
    
    if($timecreated <= strtotime('-30 days')){
@@ -116,38 +111,40 @@ foreach($mysubdata->usersubs_details as $subdatadetails){
      $thirty_recordtype_audio += $subdatadetails->audio_file_count;
      $thirty_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
      $thirty_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-     $thirty_puser += $subdatadetails->puser_count;
+     $thirty_puser .= $subdatadetails->pusers;
    }
    
-    $reportdata['pusers']=array_values(array(
-        array('name'=>'30','value'=>$thirty_puser),
-        array('name'=>'90','value'=>$ninety_puser),
-        array('name'=>'180','value'=>$oneeighty_puser),
-        array('name'=>'365','value'=>$threesixtyfive_puser)
-        ));
+}//end of for loop
 
-    $reportdata['record']=array_values(array(
+
+//calculate report summaries
+$reportdata['pusers']=array_values(array(
+        array('name'=>'30','value'=>count_pusers($thirty_puser)),
+        array('name'=>'90','value'=>count_pusers($ninety_puser)),
+        array('name'=>'180','value'=>count_pusers($oneeighty_puser)),
+        array('name'=>'365','value'=>count_pusers($threesixtyfive_puser))
+));
+
+$reportdata['record']=array_values(array(
         array('name'=>'30','value'=>$thirty_record),
         array('name'=>'90','value'=>$ninety_record),
         array('name'=>'180','value'=>$oneeighty_record),
         array('name'=>'365','value'=>$threesixtyfive_record)
-        ));
+));
 
-    $reportdata['recordmin']=array_values(array(
+$reportdata['recordmin']=array_values(array(
         array('name'=>'30','value'=>$thirty_recordmin),
         array('name'=>'90','value'=>$ninety_recordmin),
         array('name'=>'180','value'=>$oneeighty_recordmin),
         array('name'=>'365','value'=>$threesixtyfive_recordmin)
-        ));
+));
 
-    $reportdata['recordtype']=array_values(array(
+$reportdata['recordtype']=array_values(array(
         array('name'=>'30','video'=>$thirty_recordtype_video,'audio'=>$thirty_recordtype_audio),
         array('name'=>'90','video'=>$ninety_recordtype_video,'audio'=>$ninety_recordtype_audio),
         array('name'=>'180','video'=>$oneeighty_recordtype_video,'audio'=>$oneeighty_recordtype_audio),
         array('name'=>'365','video'=>$threesixtyfive_recordtype_video,'audio'=>$threesixtyfive_recordtype_audio),
-         ));
-   
-}
+));
 
 $plugin_types_arr = [];
 
@@ -183,3 +180,8 @@ if ($reportdata['subscription_check'] == true){
 }
 
 echo $OUTPUT->footer();
+
+function count_pusers($pusers){
+    return count(array_unique(explode(',',$pusers)));
+
+}
