6de4cb7c4e22ecd081046c5215a6cbd61b6c2d4e
justinhunt <bitwalkerjapan@gmail.com>
2021-01-12 13:26:12 +0900

    fixed usage report

--- classes/output/renderer.php
+++ classes/output/renderer.php
@@ -25,14 +25,16 @@ class renderer extends \plugin_renderer_base implements renderable {
         $mysubscription_name_txt = array();
         $mysubscriptions_names = array();
 
-        foreach($usagedata->usersubs as $subdata){
-            $subscription_name = ($subdata->subscriptionname == ' ') ? "na" : strtolower(trim($subdata->subscriptionname));
-            $mysubscription_name_txt[] = $subscription_name;
-            $mysubscriptions_names[] = $subscription_name;
-            $mysubscriptions[] = array('name'=>$subscription_name,
-                    'start_date'=>date("m-d-Y", $subdata->timemodified),
-                    'end_date'=>date("m-d-Y", $subdata->expiredate));
-        }
+        if($usagedata->usersubs) {
+            foreach ($usagedata->usersubs as $subdata) {
+                $subscription_name = ($subdata->subscriptionname == ' ') ? "na" : strtolower(trim($subdata->subscriptionname));
+                $mysubscription_name_txt[] = $subscription_name;
+                $mysubscriptions_names[] = $subscription_name;
+                $mysubscriptions[] = array('name' => $subscription_name,
+                        'start_date' => date("m-d-Y", $subdata->timemodified),
+                        'end_date' => date("m-d-Y", $subdata->expiredate));
+            }
+        }//end of if user subs
 
         $reportdata['subscription_check'] = false;
         if(count($mysubscriptions)>0){
@@ -74,46 +76,48 @@ class renderer extends \plugin_renderer_base implements renderable {
 
         $plugin_types_arr = "[";
 
-        foreach($usagedata->usersubs_details as $subdatadetails){
+        if($usagedata->usersubs_details) {
+            foreach ($usagedata->usersubs_details as $subdatadetails) {
 
-            $timecreated = $subdatadetails->timecreated;
+                $timecreated = $subdatadetails->timecreated;
 
-            //if(($timecreated > strtotime('-180 days'))&&($timecreated <= strtotime('-365 days'))) {
-            if(($timecreated >= strtotime('-365 days'))) {
-                $threesixtyfive_recordtype_video += $subdatadetails->video_file_count;
-                $threesixtyfive_recordtype_audio += $subdatadetails->audio_file_count;
-                $threesixtyfive_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
-                $threesixtyfive_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-                $threesixtyfive_puser .= $subdatadetails->pusers;
-            }
+                //if(($timecreated > strtotime('-180 days'))&&($timecreated <= strtotime('-365 days'))) {
+                if (($timecreated >= strtotime('-365 days'))) {
+                    $threesixtyfive_recordtype_video += $subdatadetails->video_file_count;
+                    $threesixtyfive_recordtype_audio += $subdatadetails->audio_file_count;
+                    $threesixtyfive_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
+                    $threesixtyfive_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
+                    $threesixtyfive_puser .= $subdatadetails->pusers;
+                }
 
-            //if(($timecreated > strtotime('-90 days'))&&($timecreated <= strtotime('-180 days'))){
-            if(($timecreated >= strtotime('-180 days'))){
-                $oneeighty_recordtype_video += $subdatadetails->video_file_count;
-                $oneeighty_recordtype_audio += $subdatadetails->audio_file_count;
-                $oneeighty_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
-                $oneeighty_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-                $oneeighty_puser .= $subdatadetails->pusers;
-            }
+                //if(($timecreated > strtotime('-90 days'))&&($timecreated <= strtotime('-180 days'))){
+                if (($timecreated >= strtotime('-180 days'))) {
+                    $oneeighty_recordtype_video += $subdatadetails->video_file_count;
+                    $oneeighty_recordtype_audio += $subdatadetails->audio_file_count;
+                    $oneeighty_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
+                    $oneeighty_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
+                    $oneeighty_puser .= $subdatadetails->pusers;
+                }
 
-            //if(($timecreated > strtotime('-30 days'))&&($timecreated <= strtotime('-90 days'))){
-            if(($timecreated >= strtotime('-90 days'))){
-                $ninety_recordtype_video += $subdatadetails->video_file_count;
-                $ninety_recordtype_audio += $subdatadetails->audio_file_count;
-                $ninety_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
-                $ninety_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-                $ninety_puser .= $subdatadetails->pusers;
-            }
+                //if(($timecreated > strtotime('-30 days'))&&($timecreated <= strtotime('-90 days'))){
+                if (($timecreated >= strtotime('-90 days'))) {
+                    $ninety_recordtype_video += $subdatadetails->video_file_count;
+                    $ninety_recordtype_audio += $subdatadetails->audio_file_count;
+                    $ninety_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
+                    $ninety_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
+                    $ninety_puser .= $subdatadetails->pusers;
+                }
 
-            if($timecreated >= strtotime('-30 days')){
-                $thirty_recordtype_video += $subdatadetails->video_file_count;
-                $thirty_recordtype_audio += $subdatadetails->audio_file_count;
-                $thirty_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
-                $thirty_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
-                $thirty_puser .= $subdatadetails->pusers;
-            }
+                if ($timecreated >= strtotime('-30 days')) {
+                    $thirty_recordtype_video += $subdatadetails->video_file_count;
+                    $thirty_recordtype_audio += $subdatadetails->audio_file_count;
+                    $thirty_recordmin += ($subdatadetails->audio_min + $subdatadetails->video_min);
+                    $thirty_record += ($subdatadetails->video_file_count + $subdatadetails->audio_file_count);
+                    $thirty_puser .= $subdatadetails->pusers;
+                }
 
-        }//end of for loop
+            }//end of for loop
+        }//end of if usagedata
 
 
         //calculate report summaries
@@ -147,18 +151,20 @@ class renderer extends \plugin_renderer_base implements renderable {
 
         $plugin_types_arr = [];
 
-        foreach($usagedata->usersubs_details as $subdatadetails){
-            $json_arr = json_decode($subdatadetails->file_by_app,TRUE);
-            foreach($json_arr as $key => $val) {
-                $label = $key;
-                $val = $json_arr[$key]['audio']+$json_arr[$key]['video'];
-                if(isset($plugin_types_arr[$label])) {
-                    $plugin_types_arr[$label] += $val;
-                }else{
-                    $plugin_types_arr[$label] = $val;
+        if($usagedata->usersubs_details) {
+            foreach ($usagedata->usersubs_details as $subdatadetails) {
+                $json_arr = json_decode($subdatadetails->file_by_app, true);
+                foreach ($json_arr as $key => $val) {
+                    $label = $key;
+                    $val = $json_arr[$key]['audio'] + $json_arr[$key]['video'];
+                    if (isset($plugin_types_arr[$label])) {
+                        $plugin_types_arr[$label] += $val;
+                    } else {
+                        $plugin_types_arr[$label] = $val;
+                    }
                 }
             }
-        }
+        }//end of if usersubs details
 
         echo $this->output->render_from_template('filter_poodll/mysubscriptionreport', $reportdata);
 
