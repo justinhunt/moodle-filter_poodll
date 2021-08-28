60a3da66b50e5e9eb6d42f2a94cf731840bca0b8
justinhunt <bitwalkerjapan@gmail.com>
2021-08-28 22:20:18 +0900

    mainly fixes for file extensions of whiteboard drawings

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021062800;
+$plugin->version = 2021082700;
 $plugin->requires = 2016052300;//moodle 3.1.0
 $plugin->component = 'filter_poodll';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.48 (Build 2021062800)';
+$plugin->release = '3.1.49 (Build 2021082700)';
 
