a72ec2fb13a4cf4df364e875d8cd3eddfa20b75a
Justin Hunt <bitwalkerjapan@gmail.com>
2025-04-25 20:18:13 +0900

    version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024100701;
+$plugin->version = 2024100702;
 $plugin->requires = 2016052300; //moodle 3.1.0
 $plugin->component = 'filter_poodll';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '3.1.82 (Build 2024100701)';
+$plugin->release = '3.1.83 (Build 2024100702)';
