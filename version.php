96e8b530022b5253ac45eaedd518600646afad3d
Peter Sistrom <petersistrom@catalyst-au.net>
2026-03-31 13:09:03 +1100

    Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026012700;
-$plugin->requires = 2016052300; //moodle 3.1.0
+$plugin->version = 2026033100;
+$plugin->requires = 2016052300;//moodle 3.1.0
 $plugin->component = 'filter_poodll';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '3.1.91 (Build 2026012700)';
