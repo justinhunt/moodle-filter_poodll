992dcf6b5d478047886a5be700fbc099cf1c305a
Peter Sistrom <petersistrom@catalyst-au.net>
2026-04-09 17:17:00 +1000

    Issue #44: Import core libs

--- classes/external.php
+++ classes/external.php
@@ -13,12 +13,9 @@ global $CFG;
 //This is for pre M4.0 and post M4.0 to work on same code base
 require_once($CFG->libdir . '/externallib.php');
 
-/*
- * This is for M4.0 and later
 use core_external\external_api;
 use core_external\external_function_parameters;
 use core_external\external_value;
-*/
 
 use filter_poodll\poodlltools;
 use filter_poodll\constants;
